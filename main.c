2ebac48dea6b5de6cb563a7766953789ad6fd101
Mjturn <Mjturn@outlook.com>
2024-05-04 20:10:05 -0400

    construct array of teams

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void generate_schedule(char** teams, int teams_length) {
+}
+
+int main() {
+    char* teams[] = {
+        "Los Angeles Angels",
+        "Houston Astros",
+        "Oakland Athletics",
+        "Toronto Blue Jays",
+        "Atlanta Braves",
+        "Milwaukee Brewers",
+        "St. Louis Cardinals",
+        "Chicago Cubs",
+        "Arizona Diamondbacks",
+        "Los Angeles Dodgers",
+        "San Francisco Giants",
+        "Cleveland Guardians",
+        "Seattle Mariners",
+        "Miami Marlins",
+        "New York Mets",
+        "Washington Nationals",
+        "Baltimore Orioles",
+        "San Diego Padres",
+        "Philadelphia Phillies",
+        "Pittsburgh Pirates",
+        "Tampa Bay Rays",
+        "Cincinnati Reds",
+        "Boston Red Sox",
+        "Texas Rangers",
+        "Colorado Rockies",
+        "Kansas City Royals",
+        "Detroit Tigers",
+        "Minnesota Twins",
+        "Chicago White Sox",
+        "New York Yankees",
+    };
+    
+    int teams_length = sizeof(teams) / sizeof(teams[0]);
+    return 0;
+}
